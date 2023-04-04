34a3e45700d91f7e44fcddf21c2a7ed85a58bb5c
Syed <syshah@cloudpareto.com>
2023-04-04 17:15:53 -0400

    Implementation of RefreshToken/AccessToken creation and parsing logic

--- twitter/twitter.go
+++ twitter/twitter.go
@@ -3,7 +3,8 @@ package twitter
 import "errors"
 
 var (
-	ErrBadCredentials = errors.New("incorrect email/password combination")
-	ErrNotFound       = errors.New("not found")
-	ErrValidation     = errors.New("validation error")
+	ErrBadCredentials     = errors.New("incorrect email/password combination")
+	ErrNotFound           = errors.New("not found")
+	ErrValidation         = errors.New("validation error")
+	ErrInvalidAccessToken = errors.New("invalid access token")
 )
