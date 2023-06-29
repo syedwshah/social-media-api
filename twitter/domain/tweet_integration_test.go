eeef17e2287285ae54855ff93b78a25d1e57fa79
Syed <syshah@cloudpareto.com>
2023-06-29 19:13:44 -0400

    Crete TweetService and TweetRepo interface with tests

---
+++ twitter/domain/tweet_integration_test.go
@@ -0,0 +1,48 @@
+//go:build integration
+// +build integration
+
+package domain
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/syedwshah/twitter"
+	"github.com/syedwshah/twitter/faker"
+	"github.com/syedwshah/twitter/test_helpers"
+)
+
+func TestIntegrationTweetService_Create(t *testing.T) {
+	t.Run("not auth user cannot create a tweet", func(t *testing.T) {
+		ctx := context.Background()
+
+		_, err := tweetService.Create(ctx, twitter.CreateTweetInput{
+			Body: "hello",
+		})
+
+		require.ErrorIs(t, err, twitter.ErrUnauthenticated)
+	})
+
+	t.Run("can create a tweet", func(t *testing.T) {
+		ctx := context.Background()
+
+		defer test_helpers.TeardownDB(ctx, t, db)
+
+		currentUser := test_helpers.CreateUser(ctx, t, userRepo)
+
+		ctx = test_helpers.LoginUser(ctx, t, currentUser)
+
+		input := twitter.CreateTweetInput{
+			Body: faker.RandStr(100),
+		}
+
+		tweet, err := tweetService.Create(ctx, input)
+		require.NoError(t, err)
+
+		require.NotEmpty(t, tweet.ID, "tweet.ID")
+		require.Equal(t, input.Body, tweet.Body, "tweet.Body")
+		require.Equal(t, currentUser.ID, tweet.UserID, "tweet.UserID")
+		require.NotEmpty(t, tweet.CreatedAt, "tweet.CreatedAt")
+	})
+}
--- twitter/test_helpers/test_helpers.go
+++ twitter/test_helpers/test_helpers.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/syedwshah/twitter"
+	"github.com/syedwshah/twitter/faker"
 	"github.com/syedwshah/twitter/postgres"
 )
 
@@ -14,4 +16,23 @@ func TeardownDB(ctx context.Context, t *testing.T, db *postgres.DB) {
 	err := db.Truncate(ctx)
 	require.NoError(t, err)
 
+}
+
+func CreateUser(ctx context.Context, t *testing.T, userRepo twitter.UserRepo) twitter.User {
+	t.Helper()
+
+	user, err := userRepo.Create(ctx, twitter.User{
+		Username: faker.Username(),
+		Email:    faker.Email(),
+		Password: faker.Password,
+	})
+	require.NoError(t, err)
+
+	return user
+}
+
+func LoginUser(ctx context.Context, t *testing.T, user twitter.User) context.Context {
+	t.Helper()
+
+	return twitter.PutUserIDIntoContext(ctx, user.ID)
 }
