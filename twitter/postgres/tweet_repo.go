715a8f75140952dc88119bf4a8c4205ff30cdef5
Syed <syshah@cloudpareto.com>
2023-07-02 22:43:25 -0400

    Create Tweet Migration and Setup of Create Tweet logic

--- twitter/postgres/tweet_repo.go
+++ twitter/postgres/tweet_repo.go
@@ -2,7 +2,10 @@ package postgres
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/georgysavva/scany/v2/pgxscan"
+	"github.com/jackc/pgx/v5"
 	"github.com/syedwshah/twitter"
 )
 
@@ -20,7 +23,34 @@ func (tr *TweetRepo) All(ctx context.Context) ([]twitter.Tweet, error) {
 }
 
 func (tr *TweetRepo) Create(ctx context.Context, tweet twitter.Tweet) (twitter.Tweet, error) {
-	panic("not implemented") // TODO: Implement
+	tx, err := tr.DB.Pool.Begin(ctx)
+	if err != nil {
+		return twitter.Tweet{}, fmt.Errorf("error starting transaction: %v", err)
+	}
+	defer tx.Rollback(ctx)
+
+	tweet, err = createTweet(ctx, tx, tweet)
+	if err != nil {
+		return twitter.Tweet{}, err
+	}
+
+	if err := tx.Commit(ctx); err != nil {
+		return twitter.Tweet{}, fmt.Errorf("error commiting: %v", err)
+	}
+
+	return tweet, nil
+}
+
+func createTweet(ctx context.Context, tx pgx.Tx, tweet twitter.Tweet) (twitter.Tweet, error) {
+	query := `INSERT INTO tweets (body, user_id) VALUES ($1, $2) RETURNING *;`
+
+	t := twitter.Tweet{}
+
+	if err := pgxscan.Get(ctx, tx, &t, query, tweet.Body, tweet.UserID); err != nil {
+		return twitter.Tweet{}, fmt.Errorf("error insert: %v", err)
+	}
+
+	return t, nil
 }
 
 func (tr *TweetRepo) GetByID(ctx context.Context, id string) (twitter.Tweet, error) {
